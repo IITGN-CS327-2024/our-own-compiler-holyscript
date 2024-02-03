1f7ddd21f70fe9e6a2d5058a9d85a777ca9aa36a
Yash Kokane <76534229+cocokane@users.noreply.github.com>
2024-02-04 00:14:00 +0530

    Added keywords to lexer.py

--- lexer.py
+++ lexer.py
@@ -1,1 +1,13 @@
 
+keywords = "summon doom eternal belief else chant pledge oath preach invoke deliver persist retreat trial mercy condemn unite slice".split()
+type_keywords = "int float double char bool".split()
+
+symbolic_operators = "+ - * / < > <= >= == != && || !".split()  # Assuming &&, ||, ! as logical and, or, not
+# word_operators = "and or not".split()  # Commented out because we've not mentioned this in our documentation yet, will likely not implement
+whitespace = [" ", "\n"]
+symbols = "; , ( ) { } [ ] ' \" .".split()
+
+list_utils = "cons head tail append insert remove length".split()
+array_utils = "head append remove length".split()
+tuple_utils = "length".split()  # Assuming you might want to access parts but not modify
+
