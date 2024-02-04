4270850a34c11b04a49b841af4c3f401471d687f
aishwaryaomar <76568850+aishwaryaomar@users.noreply.github.com>
2024-02-04 16:17:09 +0530

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -7,11 +7,11 @@ from core import RuntimeEnvironment
 
 
 keywords = "summon doom eternal belief else chant pledge oath preach invoke deliver persist retreat trial mercy condemn unite slice".split()
-type_keywords = "int float double char bool".split()
+type_keywords = "int float double char bool str".split()
 
-symbolic_operators = "+ - * / < > <= >= == != && || !".split()  # Assuming &&, ||, ! as logical and, or, not
+symbolic_operators = "+ - * / < > <= >= == != && || ! ?".split()  # Assuming &&, ||, ! as logical and, or, not
 # word_operators = "and or not".split()  # Commented out because we've not mentioned this in our documentation yet, will likely not implement
-whitespace = [" ", "\n"]
+whitespace = [" ", "\n", "\f", "\t", "\r", "\v"]
 symbols = "; , ( ) { } [ ] ' \" .".split()
 
 list_utils = "cons head tail append insert remove length".split()
