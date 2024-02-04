fbaff3c88f8b2b2c72789c44c10330c4b9252b2a
aishwaryaomar <76568850+aishwaryaomar@users.noreply.github.com>
2024-02-04 15:48:58 +0530

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,3 +1,10 @@
+from fractions import Fraction
+from dataclasses import dataclass
+from typing import Optional, NewType
+from utils.errors import EndOfStream, EndOfTokens, TokenError, StringError, ListOpError
+from utils.datatypes import Num, Bool, Keyword, Symbols, ListUtils, Identifier, StringToken, ListToken, Operator, Whitespace, NumLiteral, BinOp, UnOp, Variable, Let, Assign, If, BoolLiteral, UnOp, ASTSequence, AST, Buffer, ForLoop, Range, Declare, While, DoWhile, Print, funct_call, funct_def, funct_ret, StringLiteral, StringSlice, ListObject, ListCons, ListOp, ListIndex
+from core import RuntimeEnvironment
+
 
 keywords = "summon doom eternal belief else chant pledge oath preach invoke deliver persist retreat trial mercy condemn unite slice".split()
 type_keywords = "int float double char bool".split()
@@ -10,4 +17,9 @@ symbols = "; , ( ) { } [ ] ' \" .".split()
 list_utils = "cons head tail append insert remove length".split()
 array_utils = "head append remove length".split()
 tuple_utils = "length".split()  # Assuming you might want to access parts but not modify
+
+r = RuntimeEnvironment()
+
+# Token Definitions
+Token = Num | Bool | Keyword | Identifier | Operator | Symbols | StringToken | ListToken | Whitespace
 
